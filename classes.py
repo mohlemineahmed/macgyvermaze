03aeaedb55d9d6119ef335f8a05a31016244b392
Mohamed Lemine A <moiter.zom@gmail.com>
2020-06-16 18:27:45 +0200

    Création de la classe Maze et de la methode constructeur

--- classes.py
+++ classes.py
@@ -4,3 +4,11 @@ import os #Import the os module
 import random #Import the random module
 from pygame.locals import *
 from constantes import *
+
+class Maze:
+
+    def __init__(self, file):
+
+        self.file = file
+
+        self.structure = 0
