0390f9441d021a89a801852a51b297b1a7d469cd
Mohamed Lemine A <moiter.zom@gmail.com>
2020-06-16 18:31:54 +0200

    Création de la fonction generate qui va generer un labyrinthe à partir d'un fichier txt

--- classes.py
+++ classes.py
@@ -12,3 +12,12 @@ class Maze:
         self.file = file
 
         self.structure = 0
+    def generate(self):
+        with open(self.file, 'r') as file:  # to read the file
+            maze_structure = []  # Liste list which will contain each line in a list form
+            for line in file:
+                maze_line = []  # List for the lines
+                for sprite in line:  # Pour each character in a line
+                    if sprite != '\n':  # If the character is not a back to line
+                        maze_line.append(sprite) # Adds the character to the maze_line list
+                maze_structure.append(maze_line) # Adds the line as a list to maze_structure
