5e483b2e437b5ffd2c35d7aee2ce14112db677d7
Mohamed Lemine A <moiter.zom@gmail.com>
2020-06-16 18:24:28 +0200

    Ajout des imports dans le fichier classes.py

---
+++ classes.py
@@ -0,0 +1,6 @@
+import pygame  # Import pygame
+import constantes #Import the constants
+import os #Import the os module
+import random #Import the random module
+from pygame.locals import *
+from constantes import *
