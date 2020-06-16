2ac4344fa636a8e15728b47c7e53edfca6610794
Mohamed Lemine A <moiter.zom@gmail.com>
2020-06-16 19:19:28 +0200

    Initialisation de pygame et ajout des variables pour les boucles

--- game.py
+++ game.py
@@ -1,2 +1,12 @@
 import classes
 from classes import *
+pygame.init()
+window = pygame.display.set_mode((window_size, window_size))
+labyrinth = Maze('maze.txt')
+labyrinth.generate()
+continueGame= True
+MacGyver = MacGyver(mcgyver_img, labyrinth)
+compteurA = 0
+continueMain = True
+youwon = pygame.image.load(win_img).convert()
+youlost = pygame.image.load(lose_img).convert()
