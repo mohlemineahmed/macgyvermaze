b3c30628e48440eb5d12fbab828cb5d442383a84
Mohamed Lemine A <moiter.zom@gmail.com>
2020-06-16 18:33:37 +0200

    Ajout des objets à des positions random où y'a de l'air

--- classes.py
+++ classes.py
@@ -21,3 +21,23 @@ class Maze:
                     if sprite != '\n':  # If the character is not a back to line
                         maze_line.append(sprite) # Adds the character to the maze_line list
                 maze_structure.append(maze_line) # Adds the line as a list to maze_structure
+            # To place the objects randomly
+            randlist1 = random.choice(maze_structure)
+            randindex1 = maze_structure.index(randlist1)
+            randspace1 = [i for i, a in enumerate(randlist1) if a == "0"]
+            randlist1[random.choice(randspace1)] = 'A'
+            maze_structure[randindex1] = randlist1
+
+            randlist2 = random.choice(maze_structure)
+            randindex2 = maze_structure.index(randlist2)
+            randspace2 = [i for i, a in enumerate(randlist2) if a == "0"]
+            randlist2[random.choice(randspace2)] = 'B'
+            maze_structure[randindex2] = randlist2
+
+            randlist3 = random.choice(maze_structure)
+            randindex3 = maze_structure.index(randlist3)
+            randspace3 = [i for i, a in enumerate(randlist3) if a == "0"]
+            randlist3[random.choice(randspace3)] = 'C'
+            maze_structure[randindex3] = randlist3
+
+        self.structure = maze_structure  # Sauvegarde
